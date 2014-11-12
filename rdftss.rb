62e6a54d73efb8ef114fbf16bfecc4c2e7d2820e
Yusuke Komiyama <ykomiyama@bi.a.u-tokyo.ac.jp>
2014-11-12 23:20:48 +0900

    results of BH2014 DAY3 PM

--- rdftss.rb
+++ rdftss.rb
@@ -34,11 +34,16 @@ client.query("select * from tss_bincount_9606_chr18_LC2ad limit 1").each do |col
  #   up = RDF::Vocabulary.new("http://purl.uniprot.org/core/")
  #   upr = RDF::Vocabulary.new("http://purl.uniprot.org/uniprot/")
     rdf = RDF::Vocabulary.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+    rdfs = RDF::Vocabulary.new("http://www.w3.org/TR/rdf-schema/#")
     idorg = RDF::Vocabulary.new("http://info.identifiers.org/")
     faldo = RDF::Vocabulary.new("http://biohackathon.org/resource/faldo#")
     obo = RDF::Vocabulary.new("http://purl.obolibrary.org/obo/")
     efo = RDF::Vocabulary.new("http://www.ebi.ac.uk/efo/")
     tmo = RDF::Vocabulary.new("http://www.w3.org/2001/sw/hcls/ns/transmed/")
+    dcterms = RDF::Vocabulary.new("http://purl.org/dc/terms/")
+    ncit = RDF::Vocabulary.new("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#")
+    owl = RDF::Vocabulary.new("http://www.w3.org/2002/07/owl#")
+    sio = RDF::Vocabulary.new("http://semanticscience.org/resource/")
  #   ut = RDF::Vocabulary.new("http://utprot.net/")
 
     #########################################################
@@ -60,7 +65,7 @@ client.query("select * from tss_bincount_9606_chr18_LC2ad limit 1").each do |col
 #	print dbtss_uri + "\n"
 
 	dbtss_tss = dbtss.to_s + tss_ver + "/" + tissue.to_s + "/" + col1.to_s + "/" + gversion + "-" + col3.to_s + ":" + col4.to_s + ":" + strand.to_s  + "::" 
-	dbtss_tss_uri =  RDF::URI.new(dbtss_tss)
+	dbtss_tss_uri = RDF::URI.new(dbtss_tss)
 
 #	print dbtss_tss + "\n"
 
@@ -82,19 +87,31 @@ client.query("select * from tss_bincount_9606_chr18_LC2ad limit 1").each do |col
 
     graph.insert([bnode1, faldo.reference, bnode2])
 
-    graph.insert([bnode2, RDF::DC.identifier, col3])
+    graph.insert([bnode2, dcterms.identifier, col3])
 #    graph.insert([bnode2, obo.RO_0002162, idorg.taxonomy/9606])
     graph.insert([bnode2, tsso.assembly, gversion])
  
 
 #=begin
     # TSS gene
+    bnode3 = RDF::Node.uuid.to_s.insert(0,"dbtss_ver").delete("-").delete("_:")
+    bnode3 = RDF::Node.new(bnode3)
+    bnode4 = RDF::Node.uuid.to_s.insert(0,"dbtss_rep").delete("-").delete("_:")
+    bnode4 = RDF::Node.new(bnode4)
+    bnode5 = RDF::Node.uuid.to_s.insert(0,"dbtss_cnt").delete("-").delete("_:")
+    bnode5 = RDF::Node.new(bnode5)
+
     graph.insert([dbtss_tss_uri, rdf.type, obo.SO_0000315])
-    graph.insert([dbtss_tss_uri, RDF::DC.identifier, dbtss_uri])
-    graph.insert([dbtss_tss_uri, tsso.version, tss_ver])
-    graph.insert([dbtss_tss_uri, tsso.count, col6])
-    graph.insert([dbtss_tss_uri, tsso.source, efo.EFO_0003140])
-    graph.insert([dbtss_tss_uri, tsso.represent, col9])
+    graph.insert([dbtss_tss_uri, dcterms.hasVersion, bnode3])
+    	graph.insert([bnode3, rdf.type, edam.data_1670])
+    	graph.insert([bnode3, rdfs.label, tss_ver])
+    graph.insert([dbtss_tss_uri, owl.hasValue, bnode4])
+	graph.insert([bnode4, rdf.type, ncit.C67045])
+    	graph.insert([bnode4, rdfs.label, col9])
+    graph.insert([dbtss_tss_uri, owl.hasValue, bnode5])
+	graph.insert([bnode5, rdf.type, sio.SIO_000794])
+    	graph.insert([bnode5, rdfs.label, col6])
+    graph.insert([dbtss_tss_uri, dcterms.source, efo.EFO_0003140])
 
     graph.insert([dbtss_tss_uri, faldo.location, dbtss_uri])
 #=end
